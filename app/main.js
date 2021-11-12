510f19fff9bfe2a11ace5f099ede909ba437141a
Holger <jacques.hogge@hotmail.com>
2021-11-12 20:46:07 +0100

    update site to https

--- app/main.js
+++ app/main.js
@@ -1,8 +1,15 @@
 // Import module
 var express = require('express');
-var app = express()
+var app = express();
+var https = require('https');
+var fs = require('fs');
 
 
 // Start the site
 app.use(express.static('static'));
-app.listen(8080);
+https.createServer({
+    key : fs.readFileSync('./key.pem'),
+    cert : fs.readFileSync('./cert.pem'),
+    passphrase : "HiddenPlaces"
+}, app).listen(8080);
+console.log("---- Site: https://localhost:8080/index.html ----")
